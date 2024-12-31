ee20b0faa4e04ae9e1cf2da426595823787d4211
aipgdgd <poison9053@gmail.com>
2024-12-31 20:57:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7763566676:AAFPUFSFHkdnPppDy5ov6tumAsWuTcgltUU")
+BOT_TOKEN=("7743563287:AAEUCju9JoHWQoWV-Jg-ul6hBT3h3PYEDdM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mr_sid_001")
+OWNER_USERNAME=("@HUCKER777636")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1329951770")
+ADMIN_IDS=("6346283249")
 
 
 
